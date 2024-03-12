e2b5441ce6e0488b905cfcd317693ea4b9a77177
JazCz <2508413494@qq.com>
2024-03-12 14:43:21 +0800

    更新了ABCDEF类型，修复了一个问题

--- src/paixingfor6.java
+++ src/paixingfor6.java
@@ -1,18 +1,17 @@
-
-public class paixingfor5 extends paixingfor4{
-    int inserte;
-    public paixingfor5(){
-        super();
+public class paixingfor6 extends paixingfor5 {
+    int insertf;
+    public paixingfor6(){super();
     }
-    public paixingfor5(int least,int mid, int max, int insert,int inserte){
+    public paixingfor6(int least,int mid, int max, int insert,int inserte,int insertf){
         super.least=least;
         super.mid=mid;
         super.max=max;
         super.insert=insert;
-        this.inserte=inserte;
+        super.inserte=inserte;
+        this.insertf=insertf;
     }
     public void paixu(){
-        int[] pos=sort(least,mid,max,insert,inserte,1);
+        int[] pos=sort(least,mid,max,insert,inserte,insertf,1);
         System.out.print("---"+"现在牌型是");
         for(int j=0;j <pos.length;j++) {
             if (j==pos.length-1)
@@ -25,7 +24,7 @@ public class paixingfor5 extends paixingfor4{
     public void value(){
         paixu();
         int[]value=getvalue();
-        int[]pos=sort(least,mid,max,insert,inserte,1);
+        int[]pos=sort(least,mid,max,insert,inserte,insertf,1);
         for (int i = 0; i < pos.length; i++) {
             if (i==pos.length-1)
                 System.out.print(pos[i]+"的价值是"+value[pos[i]-1]+"---");
@@ -34,20 +33,20 @@ public class paixingfor5 extends paixingfor4{
         }
     }
     public int[] getarr(){
-        return sort(least,mid,max,insert,inserte,0);
+        return sort(least,mid,max,insert,inserte,insertf,0);
     }
-    @Override
-    public int[] getvalue() {
+    public int[] getvalue(){
         int[]arr=getarr();//把数初始化进入数组//
-        int[] value={f1,f1*2,f1*3,f1*4,f1*5,f1*4,f1*3,f1*2, f1};//获得least,mid,max,insert的value数组和初始化的value数组//
-        int[]pos=sort(least,mid,max,insert,inserte,1);
+        int[] value={f1,f1*2,f1*3,f1*4,f1*5,f1*4,f1*3,f1*2, f1};//获得least,mid,max,insert,inserte的value数组和初始化的value数组//
+        int[]pos=sort(least,mid,max,insert,inserte,insertf,1);
         value[least-1]+=f7;
         value[mid-1]+=f7;
         value[max-1]+=f7;
         value[insert-1]+=f7;
         value[inserte-1]+=f7;
+        value[insertf-1]+=f7;
         //设置卡张value//
-        for (int i = 0; i <= 2; i++) {
+        for (int i = 0; i <= 3; i++) {
             modifychown(arr,value,pos[i],pos[i+1],pos[i+2]);
         }
         //设置邻近因数//
@@ -61,15 +60,17 @@ public class paixingfor5 extends paixingfor4{
         modifyright(arr,value,insert);
         modifyleft(arr,value,inserte);
         modifyright(arr,value,inserte);
+        modifyleft(arr,value,insertf);
+        modifyright(arr,value,insertf);
         //设置顺张value//
-        for (int i = 0; i <=2; i++) {
+        for (int i = 0; i <=3; i++) {
             modifychowp(value,pos[i],pos[i+1],pos[i+2]);
         }
         return value;
     }
     public void drop(){
         int[] value=getvalue();
-        int[] pos=sort(least,mid,max,insert,inserte,1);
+        int[] pos=sort(least,mid,max,insert,inserte,insertf,1);
         int dropvalue=pos[0];
         for (int i = 1; i < pos.length; i++) {
             if(value[pos[i]-1]<value[dropvalue-1]){
@@ -78,9 +79,10 @@ public class paixingfor5 extends paixingfor4{
         }
         System.out.println("应当丢弃"+dropvalue+"这张牌"+"---");
     }
+
     public boolean check(){
-        int[] pos=sort(least,mid,max,insert,inserte,1);
-        for (int i = 0; i <=2; i++) {
+        int[] pos=sort(least,mid,max,insert,inserte,insertf,1);
+        for (int i = 0; i <=3; i++) {
             if(checknow(pos[i],pos[i+1],pos[i+2])==2)
             {
                 return false;
@@ -88,14 +90,15 @@ public class paixingfor5 extends paixingfor4{
         }
         return true;
     }
-    public int[] sort(int least, int mid, int max, int insert, int inserte,int mode){
+    public int[] sort(int least, int mid, int max, int insert, int inserte,int insertf,int mode){
         int[] tem=new int[9];
-        int[] pos=new int[5];
+        int[] pos=new int[6];
         tem[least-1]=least;
         tem[mid-1]=mid;
         tem[max-1]=max;
         tem[insert-1]=insert;
         tem[inserte-1]=inserte;
+        tem[insertf-1]=insertf;
         int j=0;
         for (int k : tem) {
             if (k != 0) {
--- src/test_1.java
+++ src/test_1.java
@@ -7,30 +7,36 @@ public class test_1 {
             ArrayList<paixingfor4> list3=getpx4list();//创建ABCD集合//
             ArrayList<pairbfor4> list4=getpb4list();//创建AABC或AABB集合//
             ArrayList<paixingfor5> list5=getpx5list();//创建ABCDE集合//
+            ArrayList<paixingfor6> list6=getpx6list();//创建ABCDEF集合//
             //遍历//
             for (paixing paixing : list1)
             {
-                paixing.value();
-                paixing.drop();
+                //paixing.value();
+                //paixing.drop();
             }//当前设定下同色三种不同牌的丢弃策略//
             for (pairb pairb : list2)//遍历//
             {
-                pairb.value();
-                pairb.drop();
+                //pairb.value();
+                //pairb.drop();
             }//当前设定下同色一对带一张的丢弃策略//
             for (paixingfor4 paixingfor4 : list3) {
-                paixingfor4.value();
-                paixingfor4.drop();
+                //paixingfor4.value();
+                //paixingfor4.drop();
             }
             for (pairbfor4 pairbfor4:list4)
             {
-                pairbfor4.value();
-                pairbfor4.drop();
+                //pairbfor4.value();
+                //pairbfor4.drop();
             }
             for (paixingfor5 paixingfor5:list5)
             {
-                paixingfor5.value();
-                paixingfor5.drop();
+                //paixingfor5.value();
+                //paixingfor5.drop();
+            }
+            for (paixingfor6 paixingfor6:list6)
+            {
+                paixingfor6.value();
+                paixingfor6.drop();
             }
         }
     public static ArrayList<paixing> getpxlist(){
@@ -140,5 +146,39 @@ public class test_1 {
         return list;
     }
     //循环遍历全排列后多一张的基础上再多一张的排列//
+    public static ArrayList<paixingfor6> getpx6list(){
+        ArrayList<paixingfor6> list=new ArrayList<>();
+        for(int i=1;i<=7;i++)
+        {
+            for(int j=i+1;j<=8;j++)
+            {
+                for(int k=j+1;k<=9;k++)
+                {
+                    if(i+1==j&&j+1==k){
+                        continue;
+                    }
+                    else {
+                        for (int l = 1; l <=9 ; l++) {
+                            paixingfor4 pxf4=new paixingfor4(i,j,k,l);
+                            if(l!=i&&l!=j&&l!=k&&pxf4.check()){
+                                for (int m = 1; m <=9 ; m++) {
+                                    paixingfor5 pxf5=new paixingfor5(i,j,k,l,m);
+                                    if(m!=i&&m!=j&&m!=k&&m!=l&&pxf5.check()) {
+                                        for (int n = 1; n <= 9; n++) {
+                                            paixingfor6 pxf6 = new paixingfor6(i, j, k, l, m, n);
+                                            if (n != i && n != j && n != k && n != l && n != m && pxf6.check())
+                                                list.add(pxf6);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return list;
+    }
+    //循环遍历全排列后多两张的基础上再多一张的排列//
 }
 
